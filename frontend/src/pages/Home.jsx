5c674de766fd675f122ca5a87aa532a9ac057845
GodsplanElot <elotgodsplan2@gmail.com>
2025-11-22 12:45:39 +0100

    write get note function to pull note from backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
-import { useState, useEffect } from "react"
-import api from "../api"
+import { useState, useEffect } from "react";
+import api from "../api";
 
 function Home() {
-    return <div>Home</div>
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  const getNote = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => setNotes(data)).catch((err) => alert(err))
+  };
+
+  return <div>Home</div>;
 }
 
-export default Home
+export default Home;
