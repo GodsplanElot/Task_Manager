78938ef9d818c2a91905ab37f0c354b4d13cefa6
GodsplanElot <elotgodsplan2@gmail.com>
2025-11-22 09:25:51 +0100

    modify form component

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -5,16 +5,53 @@ import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 
 function Form({route, method}) {
     const [username, setUsername] = useState("")
-    const [password, setpassword] = useState("")
+    const [password, setPassword] = useState("")
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate()
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
+        setLoading(true);
         e.preventDefault()
+
+        try { 
+            const res = await api.post(route, { username, password })
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/")
+            } else {
+                navigate("/login")
+            }
+
+         }catch (error) {
+            alert(error)
+        } finally {
+            setLoading(false)
+        }
     }
 
+    const name = method === "login" ? "Login" : "Register"
+
     return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{method === "login" ? "Login" : "Register"}</h1>
+        <h1>{name}</h1>
+        <input 
+            className="form-input"
+            type="text"
+            value={username}
+            onChange={(e)=> setUsername(e.target.value)}
+            placeholder="Username"
+        />
+        <input 
+            className="form-input"
+            type="password"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+            placeholder="Password"
+        />
+
+        <button className="form-button" type="submit">
+            {name}
+        </button>
 
     </form>
 }
